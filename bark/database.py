c8413718278dade3cb01b7ae3c4138745e84f3f0
Aleksey Voko <voko.aleksey@gmail.com>
2021-02-10 17:11:21 +0400

    Added create_table() method for DatabaseManager class

--- bark/database.py
+++ bark/database.py
@@ -14,6 +14,19 @@ class DatabaseManager:
             cursor.execute(statement, values or [])
             return cursor
 
+    def create_table(self, table_name, columns):
+        columns_with_types = [
+            f'{column_name} {data_type}'
+            for column_name, data_type in columns.items()
+        ]
+        # noinspection SqlNoDataSourceInspection
+        self._execute(
+            f'''
+            CREATE TABLE IF NOT EXISTS {table_name}
+            ({', '.join(columns_with_types)});
+            '''
+        )
+
 
 if __name__ == '__main__':
     db_manager = DatabaseManager('data_base.db')
